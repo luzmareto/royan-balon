1757445dcb5172b47baa9aa3b581e8699920d182
uwayy <sembleketek@gmail.com>
2023-10-14 22:40:39 +0700

    adding whatsapp icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <section class="footer">
         <div class="share">
           <a href="#" class="fab fa-facebook-f"></a>
-          {/* <a href="#" class="fab fa-twitter"></a> */}
+          <a href="#" class="fab fa-whatsapp"></a>
           <a href="#" class="fab fa-instagram"></a>
           {/* <a href="#" class="fab fa-linkedin"></a> */}
           {/* <a href="#" class="fab fa-pinterest"></a> */}
