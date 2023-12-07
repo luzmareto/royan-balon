a835a84b00f116738ea47b64d612d787c1c2618d
luzmareto <riosasa12@gmail.com>
2023-12-07 12:55:49 +0700

    merge

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { menu } from "../Data";
+
 const Menu = () => {
+  // Bagi array menu menjadi dua bagian
+  const firstGroup = menu.slice(0, 3);
+  const secondGroup = menu.slice(3, 6);
+
   return (
     <>
       <section className="menu" id="menu">
         <h1 className="heading">
-          our <span>event</span>
+          Grand opening at <span>Hypermart citra raya</span>
         </h1>
 
+        {/* Bagian pertama dengan tiga gambar */}
         <div className="box-container">
-          {menu.map((item, index) => (
+          {firstGroup.map((item, index) => (
             <div className="box" key={index * Math.random()}>
               <img src={item.img} alt="" />
-              <h3>tasty and healty</h3>
-              {/* <div className="price">
-                $15.99 <span>20.99</span>
-              </div> */}
-              {/* <a href="#" className="btn">
-                add to cart
-              </a> */}
+              <h3>tasty and healthy</h3>
+            </div>
+          ))}
+        </div>
+
+        {/* Headline kedua dengan tiga gambar */}
+        <h1 className="heading">
+        H U T  <span>korporat</span>
+        </h1>
+        <div className="box-container">
+          {secondGroup.map((item, index) => (
+            <div className="box" key={index * Math.random()}>
+              <img src={item.img} alt="" />
+              <h3>tasty and healthy</h3>
             </div>
           ))}
         </div>
